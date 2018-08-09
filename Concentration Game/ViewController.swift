5185afe7670176bb96724cf81672d0d1d47c6dbb
Walter Morales <morales.walt@gmail.com>
2018-08-09 10:03:04 -0400

    Connected butttons to code and started to go over git basics

--- Concentration Game/ViewController.swift
+++ Concentration Game/ViewController.swift
@@ -8,18 +8,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+class ViewController: UIViewController
+{
+    var flipCount : Int = 0 {
+        didSet {
+            flipCountLabel.text = "FLIPS : \(flipCount)"
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBOutlet weak var flipCountLabel: UILabel!
+    
+    @IBOutlet var cardButton: [UIButton]!
+    
+    var emojiChoices = [ "👻","🎃","👻","🎃"]
+    
+    @IBAction func touchCard(_ sender: UIButton) {
+        flipCount += 1
+        if let cardNumber = cardButton.index(of: sender) {
+            flipCard(withEmoji: emojiChoices[cardNumber], on: sender)
+            print("cardNumber = \(cardNumber)")
+        } else {
+            print("Chosen card was not in cardButtons")
+        }
     }
-
-
+    
+    func flipCard(withEmoji emoji: String, on button: UIButton) {
+        if button.currentTitle == emoji {
+            button.setTitle("", for: UIControlState.normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 0.5553723171, blue: 0.08206297693, alpha: 1)
+        } else {
+            button.setTitle(emoji, for: UIControlState.normal)
+            button.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        }
+    }
 }
-
